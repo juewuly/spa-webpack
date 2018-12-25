da4b3e2ac211dbce22f46d4d7ab59f84965cf22a
liuyang <758897042@qq.com>
2018-12-25 23:48:42 +0800

    update: 第一课结束

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const WebpackDeepScopeAnalysisPlugin = require('webpack-deep-scope-plugin').default;
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const argv = require("yargs-parser")(process.argv.slice(2));
+const merge = require("webpack-merge")
+const _mode = argv.mode || "development";
+const _modeflag = (_mode == "production" ? true : false);
+const _mergeConfig = require(`./config/webpack.${_mode}.js`);
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 const { join } = require("path");
 const PurifyCSSPlugin = require('purifycss-webpack');
 const glob = require("glob");
-module.exports = {
+console.log("寻找文件", glob.sync(join(__dirname, './dist/*.html')));
+webpackConfig = {
     module: {
         rules: [
             {
@@ -27,12 +36,19 @@ module.exports = {
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // both options are optional
-            filename: "[name].css",
-            chunkFilename: "[id].css"
+            filename: _modeflag ? "styles/[name].[hash:5].css" : "styles/[name].css",
+            chunkFilename: _modeflag ? "styles/[id].[hash:5].css" : "styles/[id].css"
           }),
-        new PurifyCSSPlugin({
-            // Give paths to parse for rules. These should be absolute!
-            paths: glob.sync(join(__dirname, './dist/*.html')),
-        })
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "src/index.html"
+        }),
+        new CleanWebpackPlugin(['dist']),
+        // new PurifyCSSPlugin({
+        //     // Give paths to parse for rules. These should be absolute!
+        //     paths: glob.sync(join(__dirname, './dist/*.html')),
+        // })
     ]
 };
+
+module.exports = merge(_mergeConfig, webpackConfig);
